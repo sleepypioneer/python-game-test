8593962381202b9f88e70f49b7505a5205a960b1
Jessica <jessica0greene@gmail.com>
2018-03-29 23:17:11 +0200

    pong code edited and updated

--- pong.py
+++ pong.py
@@ -1,130 +1,172 @@
 import pygame, sys
 from pygame.locals import *
 
-# set up game vriables
-window_width = 400
-window_height = 300
-line_thickness = 10
-paddle_size = 50
-paddle_offset = 20
+# Number of frames per second
+# Change this value to speed up or slow down your game
+FPS = 200
 
-# set up colour variables
-black = ( 0  ,0  ,0  ) #variables inside brackets are 'tulpes'
-white = (255,255,255) #tples are lists but the value don't change
+#Global Variables to be used through our program
 
-# ball variables (x y certesian coordinates)
-#stats pisition middle of horizontal and vertical arena
-ballX = window_width/2 - line_thickness/2
-ballY = window_height/2 - line_thickness/2
+WINDOWWIDTH = 400
+WINDOWHEIGHT = 300
+LINETHICKNESS = 10
+PADDLESIZE = 50
+PADDLEOFFSET = 20
 
-#variables to track ball direction
-ballDirX = -1 ## -1 = left 1 = right
-ballDirY = -1 ## -1 = up 1 = down
+# Set up the colours
+BLACK     = (0  ,0  ,0  )
+WHITE     = (255,255,255)
 
-# Starting position in middle of game arena
-playerOnePosition = (window_height - paddle_size) /2
-playerTwoPosition = (window_height - paddle_size) /2
-
-# create rectangles for ball and paddles
-padde1= pygame.rect(paddle_offset, playerOnePosition, line_thickness,paddle_size)
-paddle2 = pygame.rect(window_width - paddle_offset - line_thickness, playerTwoPosition, line_thickness, paddle_szie)
-ball = pygame.rect(ballX, ballY, line_thickness, line_thickness)
-
-#function to draw the arena
+#Draws the arena the game will be played in. 
 def drawArena():
-    screen.fill((0,0,0))
-    #draw outline of arena
-    pygame.draw.rect(screen, white, ((0,0),(window_width, window_height)), line_thickness*2)
-        #draw centre line
-        pygame.draw.line(screen, white, ((int(window_width/2)),0),((int(window_width/2)),window_height), (int(line_thickness/4)))
+    DISPLAYSURF.fill((0,0,0))
+    #Draw outline of arena
+    pygame.draw.rect(DISPLAYSURF, WHITE, ((0,0),(WINDOWWIDTH,WINDOWHEIGHT)), LINETHICKNESS*2)
+    #Draw centre  - needed int() for divisions as they returned floats and pygame.draw.line() takes no floats for argument
+    pygame.draw.line(DISPLAYSURF, WHITE, (int(WINDOWWIDTH/2),0),(int(WINDOWWIDTH/2),WINDOWHEIGHT), int(LINETHICKNESS/5))
 
-    # Function to draw the paddles
-    def drawPaddle(paddle):
-        #stop the paddle moving too low
-        if paddle.bottom > window_height - line_thickness:
-            paddle.bottom = window_height- line_thickness
-            #stop the padde moving too high
-        elif paddle.top < line_thickness:
-            paddle.top = line_thickness
-        #draw paddle
-        pygame.draw.rect(screen, white, paddle)
 
-#Function to draw the ball
+#Draws the paddle
+def drawPaddle(paddle):
+    #Stops paddle moving too low
+    if paddle.bottom > WINDOWHEIGHT - LINETHICKNESS:
+        paddle.bottom = WINDOWHEIGHT - LINETHICKNESS
+    #Stops paddle moving too high
+    elif paddle.top < LINETHICKNESS:
+        paddle.top = LINETHICKNESS
+    #Draws paddle
+    pygame.draw.rect(DISPLAYSURF, WHITE, paddle)
+
+
+#draws the ball
 def drawBall(ball):
-    pygame.draw.rect(screen, white, ball)
+    pygame.draw.rect(DISPLAYSURF, WHITE, ball)
 
-def moveball(ball, ballDirX, ballDirY):
+#moves the ball returns new position
+def moveBall(ball, ballDirX, ballDirY):
     ball.x += ballDirX
     ball.y += ballDirY
-return ball #returns new position
+    return ball
 
-#function chekcs for collision with wall and changes ball direction
+#Checks for a collision with a wall, and 'bounces' ball off it.
+#Returns new direction
 def checkEdgeCollision(ball, ballDirX, ballDirY):
-    if ball.top == (line_thickness) or ball.bottom == (window_height - line_thickness):
-        ballDirX = ballDirY * -1
-        if ball.left == (line_thickness) or ball.right == (window_width - line_thickness):
-            ballDirX = ballDirY  * -1
-            return ballDirX, ballDirY #return new direction
+    if ball.top == (LINETHICKNESS) or ball.bottom == (WINDOWHEIGHT - LINETHICKNESS):
+        ballDirY = ballDirY * -1
+    if ball.left == (LINETHICKNESS) or ball.right == (WINDOWWIDTH - LINETHICKNESS):
+        ballDirX = ballDirX * -1
+    return ballDirX, ballDirY
 
-#Function checks if ball has hit paddle
-def checkHitBall(ball, paddle1, paddle2, ballDirx):
-    if balldirX == - 1 and paddle1.right == ball.left and paddle1.top < ball.top and paddle1.bottom > ball.bottom:
-        return -1 #return new direction (right)
+#Checks is the ball has hit a paddle, and 'bounces' ball off it.     
+def checkHitBall(ball, paddle1, paddle2, ballDirX):
+    if ballDirX == -1 and paddle1.right == ball.left and paddle1.top < ball.top and paddle1.bottom > ball.bottom:
+        return -1
     elif ballDirX == 1 and paddle2.left == ball.right and paddle2.top < ball.top and paddle2.bottom > ball.bottom:
-        return -1 #return new direction (right)
-    else:
-        return 1 # return new direction (left)
+        return -1
+    else: return 1
 
-#Funtion for AI of computer player
+#Checks to see if a point has been scored returns new score
+def checkPointScored(paddle1, ball, score, ballDirX):
+    #reset points if left wall is hit
+    if ball.left == LINETHICKNESS: 
+        return 0
+    #1 point for hitting the ball
+    elif ballDirX == -1 and paddle1.right == ball.left and paddle1.top < ball.top and paddle1.bottom > ball.bottom:
+        score += 1
+        return score
+    #5 points for beating the other paddle
+    elif ball.right == WINDOWWIDTH - LINETHICKNESS:
+        score += 5
+        return score
+    #if no points scored, return score unchanged
+    else: return score
+
+#Artificial Intelligence of computer player 
 def artificialIntelligence(ball, ballDirX, paddle2):
-    # Ball is moving awa from paddle, bove back to centre
+    #If ball is moving away from paddle, center bat
     if ballDirX == -1:
-        if paddle2.centery < (window_height/2):
-            paddle2.y +=1 
-        elif paddle2.centery > (window_height/2):
-            paddle2.y -=1
-    # Ball moving towards bat,# track its movement
+        if paddle2.centery < (WINDOWHEIGHT/2):
+            paddle2.y += 1
+        elif paddle2.centery > (WINDOWHEIGHT/2):
+            paddle2.y -= 1
+    #if ball moving towards bat, track its movement. 
     elif ballDirX == 1:
         if paddle2.centery < ball.centery:
             paddle2.y += 1
         else:
-            paddle2.y -= 1
+            paddle2.y -=1
     return paddle2
 
- #intialise the window
- #screen = pygame.display.set mode(window_width, window_height)
- #pygame.display.set_caption('Pong') #DIsplays in the window the Name Pong
+#Displays the current score on the screen
+def displayScore(score):
+    resultSurf = BASICFONT.render('Score = %s' %(score), True, WHITE)
+    resultRect = resultSurf.get_rect()
+    resultRect.topleft = (WINDOWWIDTH - 150, 25)
+    DISPLAYSURF.blit(resultSurf, resultRect)
 
- #draw the arena and paddles
- drawArena()
- drawPaddle(paddle1)
- drawPaddle(paddle2)
- drawBall(ball)
+#Main function
+def main():
+    pygame.init()
+    global DISPLAYSURF
+    ##Font information
+    global BASICFONT, BASICFONTSIZE
+    BASICFONTSIZE = 20
+    BASICFONT = pygame.font.Font('freesansbold.ttf', BASICFONTSIZE)
 
- #make curors invisible
- pygame.mouse.set_visible(0)
+    FPSCLOCK = pygame.time.Clock()
+    DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH,WINDOWHEIGHT)) 
+    pygame.display.set_caption('Pong')
 
- #main game runs in this loop
- while True: #infinite loop.presss CTRL-C to quite Game
- for event in pygame.event.get():
-    if event.type == Quit:
-        pygame.quit()
-        sys.exit()
-        #mouse movement
-    elif even.type == MOUSMOTION:
-        mousex, mousey = event.pos
-        paddle1.y = mousey
+    #Initiate variable and set starting positions
+    #any future changes made within rectangles
+    ballX = WINDOWWIDTH/2 - LINETHICKNESS/2
+    ballY = WINDOWHEIGHT/2 - LINETHICKNESS/2
+    playerOnePosition = (WINDOWHEIGHT - PADDLESIZE) /2
+    playerTwoPosition = (WINDOWHEIGHT - PADDLESIZE) /2
+    score = 0
 
+    #Keeps track of ball direction
+    ballDirX = -1 ## -1 = left 1 = right
+    ballDirY = -1 ## -1 = up 1 = down
+
+    #Creates Rectangles for ball and paddles.
+    paddle1 = pygame.Rect(PADDLEOFFSET,playerOnePosition, LINETHICKNESS,PADDLESIZE)
+    paddle2 = pygame.Rect(WINDOWWIDTH - PADDLEOFFSET - LINETHICKNESS, playerTwoPosition, LINETHICKNESS,PADDLESIZE)
+    ball = pygame.Rect(ballX, ballY, LINETHICKNESS, LINETHICKNESS)
+
+    #Draws the starting position of the Arena
     drawArena()
     drawPaddle(paddle1)
     drawPaddle(paddle2)
     drawBall(ball)
 
-    ball = moveBall(ball, ballDirX, ballDirY)
-    ballDirX = ballDirY = CheckEdgeCollision(ball, ballDirX, ballDirY)
-    ballDirX = ballDirY * checkHitball(ball, paddle1, paddle2, ballDirX)
-    paddle2 = artificialIntelligence (ball, ballDirX, paddle1, paddle2)
-    pygame.display.update()
+    pygame.mouse.set_visible(0) # make cursor invisible
 
+    while True: #main game loop
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            # mouse movement commands
+            elif event.type == MOUSEMOTION:
+                mousex, mousey = event.pos
+                paddle1.y = mousey
 
+        drawArena()
+        drawPaddle(paddle1)
+        drawPaddle(paddle2)
+        drawBall(ball)
 
+        ball = moveBall(ball, ballDirX, ballDirY)
+        ballDirX, ballDirY = checkEdgeCollision(ball, ballDirX, ballDirY)
+        score = checkPointScored(paddle1, ball, score, ballDirX)
+        ballDirX = ballDirX * checkHitBall(ball, paddle1, paddle2, ballDirX)
+        paddle2 = artificialIntelligence (ball, ballDirX, paddle2)
+
+        displayScore(score)
+
+        pygame.display.update()
+        FPSCLOCK.tick(FPS)
+
+if __name__=='__main__':
+    main()
